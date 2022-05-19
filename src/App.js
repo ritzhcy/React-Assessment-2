d20f5699a42a6614eeddc8543310d27737cdf45f
preetichoudhury <iampreetichoudhury@gmail.com>
2022-05-19 15:04:08 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './App.css';
+import Form from "./Component/Form";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
+  }
+}
+
+export default App;
